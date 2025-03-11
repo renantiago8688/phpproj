f80da049c21635146bd6668ec601a120671ecc9e
ra2357090 <ra2357090@LDI306>
2025-03-11 20:11:04 -0300

    primeira atualizacao

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$cidade = "<b>Bauru</b>";
+$dia = "<mark>06</mark>";
+$mes = "Março";
+$ano = "2025";
+
+echo "$cidade $dia $mes $ano";
+
+/* as variaveis todas em string contendo respectivamente a cidade, dia, mes, e ano. O texto da variavel didade esta em negrito e o dia está com o fundo do texto em amarelo*/
+?>
